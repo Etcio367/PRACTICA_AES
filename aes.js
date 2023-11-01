e0fd188663a35bd65f3939f1f782b60197225626
Erick <erickdo361@gmail.com>
2023-11-01 11:07:09 -0600

    Intento de decifrar ECB

--- aes.js
+++ aes.js
@@ -40,12 +40,26 @@ function cifrar()
                 rgbInput.innerHTML = '';
 
                 const modo = document.getElementById('modoDecEnc').value;
+                const operacion = document.querySelector('input[name="operacion"]:checked').value;
 
+                console.log(operacion);
                 console.log(modo);
 
-                if(modo === "ECB");
+                if(operacion === "Cifrar")
                 {
-                    cifradorECB(pixels, rgbInput, x, imgBMP, canvas);
+                    if(modo === "ECB")
+                    {
+                        cifradorECB(pixels, rgbInput, x, imgBMP, canvas);
+                    }
+                }
+                else if(operacion === "Descifrar")
+                {
+                    console.log("lll");
+                    if(modo === "ECB")
+                    {
+                        console.log("lll");
+                        descifradoECB(pixels, rgbInput, x, imgBMP, canvas);
+                    }
                 }
 
             };          
@@ -75,14 +89,25 @@ function cifradorECB(pixels, rgbInput, x, imgBMP, canvas)
         muestraResultado.textContent = `RGB: ${conjunto}`;
         rgbInput.appendChild(muestraResultado);
 
-        console.log("hola");
+        //console.log("hola");
 
         for(let j = 0; j < conjunto.length; j += 4)
         {
+            //console.log("hola");
             var aux = conjunto[0].toString();
+            console.log(aux);
             var cif = CryptoJS.AES.encrypt(aux, pwd);
-            aux = cif.toString(CryptoJS.format.Hex);
+            console.log(cif);
+
+            aux = cif.toString().slice(0, 9);
+
+            aux = hexadecimal(aux);
+
+            //aux = cif.toString(CryptoJS.format.Hex);
+
+            console.log(aux);
             w[x] = (parseInt(aux, 16)%255);
+            console.log(w[x]);
             
             aux = conjunto[1].toString();
             cif = CryptoJS.AES.encrypt(aux, pwd);
@@ -91,6 +116,101 @@ function cifradorECB(pixels, rgbInput, x, imgBMP, canvas)
 
             aux = conjunto[2].toString();
             cif = CryptoJS.AES.encrypt(aux, pwd);
+            aux = cif.toString(CryptoJS.format.Hex);
+            z[x] = (parseInt(aux, 16)%255);
+
+            console.log(w[x], y[x], z[x]);
+
+            x ++;
+
+            var rgbSuma = document.createElement('div');
+            rgbSuma.textContent = `SUMA RGB: (${conjunto[0]}, ${conjunto[1]}, ${conjunto[2]})`
+            rgbInput.appendChild(rgbSuma);
+
+            conjunto[0] = [];
+            conjunto[1] = [];
+            conjunto[2] = [];
+
+
+        }    
+    }
+
+    const canvasDos = document.getElementById('Canvas2');
+    const ctxNuevo = canvasDos.getContext('2d');
+    
+    canvasDos.width = imgBMP.width;
+    canvasDos.height = imgBMP.height;
+
+    x = 0;
+    var i = 0;
+    var j = 0;
+
+    for(i = 0; i < canvasDos.height; i++){
+
+        for(j = 0; j < canvasDos.width; j++)
+        {
+            var r = w[x];
+            var g = y[x];
+            var b = z[x];
+
+            ctxNuevo.fillRect(j,i,canvasDos.height, canvasDos.width);
+            ctxNuevo.fillStyle = `rgb(${r}, ${g}, ${b})`;
+
+            x++;
+        }
+    }
+
+    const canvasURL = canvasDos.toDataURL('image/bmp');
+    var download = document.createElement('a');
+
+    download.href = canvasURL;
+    download.download = "img_c.bmp";
+
+    download.click();
+
+    w = [];
+    y = [];
+    z = [];
+
+}
+
+
+function descifradoECB(pixels, rgbInput, x, imgBMP, canvas){
+    
+    for(let i = 0; i < pixels.length ; i += 4){
+
+        var r = pixels[i];
+        var g = pixels[i+1];
+        var b = pixels[i+2];
+
+        console.log(pixels[i], pixels[i+1], pixels[i+2]);
+
+        const rgbTexto = document.createElement('div');
+        rgbTexto.textContent = `RGB: (${r}, ${g}, ${b})`;
+
+        const conjunto = [r,g,b];
+
+        const muestraResultado = document.createElement('div');
+        muestraResultado.textContent = `RGB: ${conjunto}`;
+        rgbInput.appendChild(muestraResultado);
+
+        console.log("xxx");
+
+        for(let j = 0; j < conjunto.length; j += 4)
+        {
+            var aux = conjunto[0].toString();
+            console.log(aux);
+            var cif = CryptoJS.AES.decrypt(aux, pwd);
+            aux = cif.toString(CryptoJS.format.Hex);
+            w[x] = (parseInt(aux, 16)%255);
+            
+            aux = conjunto[1].toString();
+            cif = CryptoJS.AES.decrypt(aux, pwd);
+            aux = cif.toString(CryptoJS.format.Hex);
+            y[x] = (parseInt(aux, 16)%255);
+
+            aux = conjunto[2].toString();
+            cif = CryptoJS.AES.decrypt(aux, pwd);
             aux = cif.toString(CryptoJS.format.Hex);
             z[x] = (parseInt(aux, 16)%255);
 
@@ -133,8 +253,17 @@ function cifradorECB(pixels, rgbInput, x, imgBMP, canvas)
     var download = document.createElement('a');
 
     download.href = canvasURL;
-    download.download = "img_c.bmp";
+    download.download = "img_c_d.bmp";
 
     download.click();
 
+}
+
+
+function hexadecimal(aux){
+    let hex = '';
+    for (let i = 0; i < aux.length; i++) {
+        hex += aux.charCodeAt(i).toString(16);
+    }
+    return hex;
 }
